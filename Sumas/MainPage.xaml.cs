00d6af36b416feef99502001d0579b192dfacfc8
erikbaca <erik.baca.uth@gmail.com>
2022-10-04 17:48:23 -0600

    Agregar archivos de proyecto.

---
+++ Sumas/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sumas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Sumar_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Numero1.Text) || (String.IsNullOrEmpty(Numero2.Text))){
+                _ = DisplayAlert("Campos Vacios", "Hay un Campo Vacio! Favor Llenar Ambos Campos", "Volver");
+            }
+            else
+            {
+          
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+            int suma = n1 + n2;
+
+            await Navigation.PushAsync(new Pagina_Resultado( suma.ToString()));
+            }
+        }
+
+        private async void Restar_Clicked(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(Numero1.Text) || (String.IsNullOrEmpty(Numero2.Text)))
+            {
+                _ = DisplayAlert("Campos Vacios", "Hay un Campo Vacio! Favor Llenar Ambos Campos", "Volver");
+            }
+            else
+            {
+
+                int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+            int resta = n1 - n2;
+
+            await Navigation.PushAsync(new Pagina_Resultado(resta.ToString()));
+            }
+        
+    }
+
+    private async void Multiplicar_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Numero1.Text) || (String.IsNullOrEmpty(Numero2.Text)))
+            {
+                _ = DisplayAlert("Campos Vacios", "Hay un Campo Vacio! Favor Llenar Ambos Campos", "Volver");
+            }
+            else
+            {
+                int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+            int multiplicar = n1 * n2;
+
+            await Navigation.PushAsync(new Pagina_Resultado(multiplicar.ToString()));
+
+        }
+        }
+
+        private async void Dividir_Clicked(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+            if (n2.Equals(0))
+            {
+                _ = DisplayAlert("Division entre Cero", "El Valor a dividir es igual a Cero, Esto Provacaria una Excepcion de Error", "Volver");
+            }
+
+           else if (String.IsNullOrEmpty(Numero1.Text) || (String.IsNullOrEmpty(Numero2.Text)))
+                {
+                    _ = DisplayAlert("Campos Vacios", "Hay un Campo Vacio! Favor Llenar Ambos Campos", "Volver");
+                }
+                else
+                {
+            
+            int dividir = n1 / n2;
+            await Navigation.PushAsync(new Pagina_Resultado(dividir.ToString()));           
+
+        }
+            
+        }
+    }
+}
---
+++ Sumas/Pagina_Resultado.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Sumas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina_Resultado : ContentPage
+    {
+        public Pagina_Resultado(String Datos)
+        {
+            InitializeComponent();
+            
+         lblresultado.Text = "El Resultado es: " + Datos;
+        
+
+        }
+    }
+}
